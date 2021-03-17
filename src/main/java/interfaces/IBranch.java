7381b5ad27b882e9687d29656c69917a01ce843a
aminhad <amin.hadi@amalitech.org>
2021-03-17 11:28:59 +0000

    Branch interface modified to include all method declaration

--- src/main/java/interfaces/IBranch.java
+++ src/main/java/interfaces/IBranch.java
@@ -1,4 +1,12 @@
 package interfaces;
 
+import classes.Customer;
+
+import java.util.ArrayList;
+
 public interface IBranch {
+    String getName();
+    ArrayList<Customer> getCustomers();
+    boolean newCustomer(String customerName, double initTransaction);
+    boolean addCustomerTransaction(String customerName, double transaction);
 }
