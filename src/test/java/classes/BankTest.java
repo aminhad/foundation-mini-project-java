b90a612345c9c699be2756a8e655bbb50840596b
aminhad <amin.hadi@amalitech.org>
2021-03-19 17:09:30 +0000

    test cases modified

--- src/test/java/classes/BankTest.java
+++ src/test/java/classes/BankTest.java
@@ -3,9 +3,13 @@ package classes;
 import static org.junit.Assert.*;
 
 public class BankTest {
+    Bank bank;
 
     @org.junit.Before
     public void setUp() throws Exception {
+        bank = new Bank("Digi Bank");
+        bank.addBranch("RaceCourse");
+        bank.addCustomer("RaceCourse", "Ali", 232.22);
     }
 
     @org.junit.After
@@ -14,17 +18,28 @@ public class BankTest {
 
     @org.junit.Test
     public void addBranch() {
+        assertTrue(bank.addBranch("soft branch"));
     }
 
     @org.junit.Test
     public void addCustomer() {
+        assertTrue(bank.addCustomer("soft branch", "Kofi", 822.33));
+
+        assertFalse(bank.addCustomer("RaceCourse", "Ali", 500.32));
     }
 
     @org.junit.Test
     public void addCustomerTransaction() {
+        assertTrue(bank.addCustomer("soft branch", "Kofi", 822.73));
+
+        assertFalse(bank.addCustomer("RaceCourse", "Ali", 500.39));
     }
 
     @org.junit.Test
     public void listCustomers() {
+
+        assertTrue(bank.listCustomers("RaceCourse", false));
+
+        assertFalse(bank.listCustomers("no branch", false));
     }
 }
--- src/test/java/classes/BranchTest.java
+++ src/test/java/classes/BranchTest.java
@@ -3,9 +3,12 @@ package classes;
 import static org.junit.Assert.*;
 
 public class BranchTest {
+    Branch branch;
 
     @org.junit.Before
     public void setUp() throws Exception {
+        branch = new Branch("citi");
+        branch.newCustomer("Samir", 283.88);
     }
 
     @org.junit.After
@@ -14,17 +17,23 @@ public class BranchTest {
 
     @org.junit.Test
     public void getName() {
+        assertEquals("citi", branch.getName());
     }
 
     @org.junit.Test
     public void getCustomers() {
+        assertEquals("Samir", branch.getCustomers().get(0).getName());
     }
 
     @org.junit.Test
     public void newCustomer() {
+        assertTrue(branch.newCustomer("Nasir", 28.22));
+        assertFalse(branch.newCustomer("Samir", 283.88));
     }
 
     @org.junit.Test
     public void addCustomerTransaction() {
+        assertTrue(branch.addCustomerTransaction("Samir", 283.88));
+        assertFalse(branch.addCustomerTransaction("Nasir", 28.22));
     }
 }
